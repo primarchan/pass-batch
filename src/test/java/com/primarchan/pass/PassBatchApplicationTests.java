ffd614305662eb857c7821bc3c576c226923c080
An T.G <boltcarrier@naver.com>
2022-10-18 20:11:44 +0900

    first commit

---
+++ src/test/java/com/primarchan/pass/PassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primarchan.pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
